ad3d3ccd995f51f345cc61519955bf4b810d1878
0x6c6868 <1023347025@qq.com>
2022-02-11 12:03:00 +0800

    Update init.py

--- init.py
+++ init.py
@@ -4,3 +4,11 @@ import os
 
 # 添加tmux配置到zsh环境
 os.system('echo "[[ \$TERM != \\"screen\\" ]] && exec tmux" >> ~/.zshrc')
+
+# 安装vim vundle
+os.system('git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim')
+
+# TODO 移动相关 vimrc 配置
+
+# 安装vim vundle插件
+os.system('vim +PluginInstall +qall')
